b625b6d26f097016afe31c93fd84faa60b73e80f
lizuoyang <lizuoyangsina@qq.com>
2021-02-17 16:15:14 +0800

    feat: 项目结构优化

--- hello-world/src/main/java/com/lizuoyang/springboot/HelloWorldApplication.java
+++ hello-world/src/main/java/com/lizuoyang/springboot/HelloWorldApplication.java
@@ -1,9 +1,14 @@
 package com.lizuoyang.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @RestController
@@ -16,6 +21,17 @@ public class HelloWorldApplication {
     @GetMapping("/hello")
     public String hello() {
         return "Hello SpringBoot!!!";
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        // 目的是
+        return args -> {
+            System.out.println("来看看 SpringBoot 默认为我们提供的 Bean：");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(System.out::println);
+        };
     }
 
 }
